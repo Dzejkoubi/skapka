c9f292425c36552afb7cbfbc4c49f96f4fa7959a
Dzejkoubi <kubakraus06@gmail.com>
2025-11-30 22:36:23 +0100

    feat: added ColorTheme and TextTheme files to define styles attempted to change appTheme added DevicePreview package

--- skapka_app/lib/screens/welcome_screen/welcome_screen.dart
+++ skapka_app/lib/screens/welcome_screen/welcome_screen.dart
@@ -1,6 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:skapka_app/app/theme/app_color_theme.dart';
+import 'package:skapka_app/app/theme/app_text_theme.dart';
 import 'package:skapka_app/l10n/app_localizations.dart';
 import 'package:skapka_app/widgets/screen_wrapper.dart';
 
@@ -12,6 +14,7 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenWrapper(
       child: Scaffold(
+        backgroundColor: context.colors.background.light,
         body: Column(
           children: [
             SizedBox(height: 64),
@@ -31,6 +34,11 @@ class WelcomeScreen extends StatelessWidget {
                 spacing: 8, // TODO: change to a variable
                 children: [
                   ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(
+                        context.colors.primary.dark,
+                      ),
+                    ),
                     onPressed: () {
                       print('login button pressed');
                     },
@@ -38,6 +46,7 @@ class WelcomeScreen extends StatelessWidget {
                       AppLocalizations.of(
                         context,
                       )!.welcome_screen_login_button_text,
+                      style: AppTextTheme.cta,
                     ),
                   ),
                   ElevatedButton(
@@ -48,6 +57,7 @@ class WelcomeScreen extends StatelessWidget {
                       AppLocalizations.of(
                         context,
                       )!.welcome_screen_register_button_text,
+                      style: AppTextTheme.cta,
                     ),
                   ),
                   ElevatedButton(
@@ -58,6 +68,7 @@ class WelcomeScreen extends StatelessWidget {
                       AppLocalizations.of(
                         context,
                       )!.welcome_screen_songbook_button_text,
+                      style: AppTextTheme.displayLarge,
                     ),
                   ),
                 ],

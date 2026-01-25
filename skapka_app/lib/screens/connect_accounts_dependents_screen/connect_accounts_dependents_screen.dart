2a09d5d7513d530c6245f8b6023d2a2526b81c5b
Dzejkoubi <kubakraus06@gmail.com>
2026-01-25 16:22:37 +0100

    refactor: move connect_accounts_dependents_account_box to a standalone file

--- skapka_app/lib/screens/connect_accounts_dependents_screen/connect_accounts_dependents_screen.dart
+++ skapka_app/lib/screens/connect_accounts_dependents_screen/connect_accounts_dependents_screen.dart
@@ -3,25 +3,18 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:skapka_app/app/l10n/l10n_extension.dart';
-import 'package:skapka_app/app/router/router.gr.dart';
 import 'package:skapka_app/app/theme/app_spacing.dart';
-import 'package:skapka_app/app/theme/app_text_theme.dart';
-import 'package:skapka_app/app/theme/main_button_theme.dart';
-import 'package:skapka_app/models/account_model.dart';
 import 'package:skapka_app/models/dependents/account_dependent_model.dart';
 import 'package:skapka_app/models/dependents/dependent_notes_model.dart';
 import 'package:skapka_app/providers/account_provider.dart';
 import 'package:skapka_app/providers/admin_panel_provider.dart';
 import 'package:skapka_app/providers/loading_provider.dart';
+import 'package:skapka_app/screens/connect_accounts_dependents_screen/widgets/connect_accounts_dependents_account_box.dart';
 import 'package:skapka_app/services/supabase_service.dart';
 import 'package:skapka_app/widgets/appbar/appbar.dart';
-import 'package:skapka_app/widgets/buttons/main_button.dart';
-import 'package:skapka_app/widgets/dialogs/bottom_dialog.dart';
-import 'package:skapka_app/widgets/dialogs/large_dialog.dart';
 import 'package:skapka_app/widgets/forms/custom_form.dart';
 import 'package:skapka_app/widgets/loading_floating_logo/centered_loading_rotating_logo.dart';
 import 'package:skapka_app/widgets/wrappers/screen_wrapper.dart';
-import 'package:skapka_app/widgets/basic_expansion_tile.dart';
 
 @RoutePage()
 class ConnectAccountsDependentsScreen extends StatelessWidget {
@@ -80,47 +73,6 @@ class ConnectAccountsDependentsScreen extends StatelessWidget {
         }
       } catch (e) {
         debugPrint('Error loading data in EditLeadersScreen: $e');
-      }
-    }
-
-    void disconnectDependentFromAccount(
-      AccountDependentModel dependent,
-      AccountModel account,
-      AdminPanelProvider adminProvider,
-      SupabaseService supabaseService,
-      LoadingProvider loadingProvider,
-      BuildContext context,
-    ) async {
-      try {
-        loadingProvider.show();
-        await supabaseService.disconnectDependentFromAccount(
-          dependentId: dependent.dependentId,
-          accountId: account.accountId,
-        );
-        await loadData(forceRefresh: true);
-        if (context.mounted) {
-          context.pop();
-          BottomDialog.show(
-            context,
-            type: BottomDialogType.positive,
-            description: context.localizations
-                .admin_panel_screen_connect_accounts_dependents_delete_connection_success(
-                  '${dependent.name} ${dependent.surname}',
-                  '${account.name} ${account.surname}',
-                ),
-          );
-        }
-      } catch (e) {
-        debugPrint('Error disconnecting dependent from account: $e');
-        if (context.mounted) {
-          BottomDialog.show(
-            context,
-            type: BottomDialogType.negative,
-            description: context.localizations.generic_error,
-          );
-        }
-      } finally {
-        loadingProvider.hide();
       }
     }
 
@@ -229,133 +181,13 @@ class ConnectAccountsDependentsScreen extends StatelessWidget {
                                       isMainDependent: relation.isMainDependent,
                                     );
                                   }).whereType<AccountDependentModel>();
-                                  return BasicExpansionTile(
-                                    title:
-                                        '${account.name} ${account.surname} (${accountDependents.length})',
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        ...accountDependents.map(
-                                          (dependent) => Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            children: [
-                                              SingleChildScrollView(
-                                                scrollDirection:
-                                                    Axis.horizontal,
-                                                child: Text(
-                                                  '${dependent.name} ${dependent.surname}',
-                                                  style: AppTextTheme.bodyLarge(
-                                                    context,
-                                                  ),
-                                                ),
-                                              ),
-                                              SizedBox(
-                                                width: AppSpacing.medium,
-                                              ),
-                                              MainButton(
-                                                style: ButtonStyleTypes.text,
-                                                text: '',
-                                                type: ButtonType.icon,
-                                                variant: ButtonStylesVariants
-                                                    .destructive,
-                                                state: dependent.isMainDependent
-                                                    ? ButtonState.disabled
-                                                    : ButtonState.released,
-                                                iconAsset:
-                                                    'assets/icons/trash.svg',
-                                                onPressed: () => showDialog(
-                                                  context: context,
-                                                  builder: (_) {
-                                                    return LargeDialog(
-                                                      type: LargeDialogType
-                                                          .negative,
-                                                      title: context
-                                                          .localizations
-                                                          .admin_panel_screen_connect_accounts_dependents_delete_connection_dialog_title,
-                                                      description: context
-                                                          .localizations
-                                                          .admin_panel_screen_connect_accounts_dependents_delete_connection_dialog_subtitle(
-                                                            '${dependent.name} ${dependent.surname}',
-                                                            '${account.name} ${account.surname}',
-                                                          ),
-                                                      primaryButtonText: context
-                                                          .localizations
-                                                          .admin_panel_screen_connect_accounts_dependents_delete_connection_dialog_title,
-                                                      secondaryButtonText:
-                                                          context
-                                                              .localizations
-                                                              .cancel,
-                                                      onPrimaryPressed: () {
-                                                        if (dependent
-                                                            .isMainDependent) {
-                                                          context.pop();
-                                                          BottomDialog.show(
-                                                            context,
-                                                            type:
-                                                                BottomDialogType
-                                                                    .negative,
-                                                            description: context
-                                                                .localizations
-                                                                .admin_panel_screen_connect_accounts_dependents_delete_connection_error_main_dependent(
-                                                                  '${dependent.name} ${dependent.surname}',
-                                                                  '${account.name} ${account.surname}',
-                                                                ),
-                                                          );
-                                                        } else {
-                                                          disconnectDependentFromAccount(
-                                                            dependent,
-                                                            account,
-                                                            adminProvider,
-                                                            supabaseService,
-                                                            loadingProvider,
-                                                            context,
-                                                          );
-                                                        }
-                                                      },
-                                                      onSecondaryPressed: () {
-                                                        context.pop();
-                                                      },
-                                                    );
-                                                  },
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                        SizedBox(height: AppSpacing.medium),
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            MainButton(
-                                              style: ButtonStyleTypes.outlined,
-                                              variant:
-                                                  ButtonStylesVariants.normal,
-                                              text: context
-                                                  .localizations
-                                                  .admin_panel_screen_connect_accounts_dependents_add_connection_button_text,
-                                              onPressed: () async {
-                                                await context.router.push(
-                                                  AddDependentsToAccountRoute(
-                                                    adminProvider:
-                                                        adminProvider,
-                                                    account: account,
-                                                    alreadyConnectedDependents:
-                                                        accountDependents
-                                                            .toList(),
-                                                  ),
-                                                );
-                                                await loadData(
-                                                  forceRefresh: true,
-                                                );
-                                              },
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
+                                  return ConnectAccountsDependentsAccountBox(
+                                    accountDependents: accountDependents,
+                                    supabaseService: supabaseService,
+                                    loadingProvider: loadingProvider,
+                                    account: account,
+                                    adminProvider: adminProvider,
+                                    loadData: loadData,
                                   );
                                 }),
                           ],
